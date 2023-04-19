abb14c082381bdcce601f4696835f2c29727b0a1
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2023-04-19 12:46:51 +0300

    run-eclair-builds.sh: Print number of configs to build
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    Change-Id: I4dd3b50d403d303967ad55b51b5d2c1b9dfb8ae5

--- run-eclair-builds.sh
+++ run-eclair-builds.sh
@@ -5,8 +5,14 @@ set -ex
 . tf-m-ci-scripts/eclair/utils.sh
 . tf-m-ci-scripts/eclair/utils_tfm.sh
 
+num_configs=$(python3 ./tf-m-ci-scripts/configs.py -g "$FILTER_GROUP" | wc -l)
+
+echo "Number of configs to build: $num_configs"
+
+cnt=1
+
 for cfg in $(python3 ./tf-m-ci-scripts/configs.py -g "$FILTER_GROUP"); do
-    echo ============== $cfg ==============
+    echo "============== $cfg ($cnt/$num_configs) =============="
     export CONFIG_NAME=$cfg
     (cd mbedtls; git checkout .; git clean -fq)
     (cd psa-arch-tests; git checkout .; git clean -fq)
@@ -14,4 +20,5 @@ for cfg in $(python3 ./tf-m-ci-scripts/configs.py -g "$FILTER_GROUP"); do
     eclair_tfm_set_toolchain_path
     detachLicense 3000
     tf-m-ci-scripts/run-build.sh
+    cnt=$((cnt + 1))
 done
