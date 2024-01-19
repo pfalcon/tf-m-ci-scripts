90434714b2ab8bef86c0b655f91625c0f57397a3
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2024-01-19 16:13:40 +0700

    run-eclair-builds.sh: Extend license after the last build for post-processing
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    Change-Id: I608f32f72e37874381af7abe4e111ee71f4cbdca

--- run-eclair-builds.sh
+++ run-eclair-builds.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+LICENSE_LEASE=5400
+
 . tf-ci-scripts/eclair/utils.sh
 . tf-m-ci-scripts/eclair/utils_tfm.sh
 
@@ -18,9 +20,13 @@ for cfg in $(python3 ./tf-m-ci-scripts/configs.py -g "$FILTER_GROUP"); do
     (cd psa-arch-tests; git checkout .; git clean -fq)
     (cd trusted-firmware-m; git checkout .; git clean -fq)
     eclair_tfm_set_toolchain_path
-    detachLicense 5400
+    detachLicense $LICENSE_LEASE
     tf-m-ci-scripts/run-build.sh
     cnt=$((cnt + 1))
 done
+
+# Extend license lease one final time for further processing after the builds
+# (creation of project database and rendering reports, etc.).
+detachLicense $LICENSE_LEASE
 
 echo "Built $num_configs configs"
